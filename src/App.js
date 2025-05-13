5708e0cf7790dd9cce53ff5465303c59e9066ca6
sixux <gm39441679@gmail.com>
2025-05-13 18:40:45 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './pages/home/HomePage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 // import GalleryPage from './pages/gallery/GalleryPage';
@@ -10,21 +10,24 @@ import AboutMePage from './pages/aboutMe/AboutMePage';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/mysite_front_sample">
         <div className="App">
           <header className="app-header">
             { <Header /> }
           </header>
           <main>
             <Routes>
-              <Route path="/" element={<HomePage />}  />
+
+              <Route path="/" element={<Navigate to="/home" />}  />
+              <Route path="/home" element={<HomePage />} />
               <Route path="/about-me" element={<AboutMePage />}/>
               {/* <Route path="/gallery" element={<GalleryPage />}/>
               <Route path="blog" element={<BlogPage />}/>
-              <Route path="my-story" element={<MyStoryPage />}/> */}
+              <Route path="my-story" element={<MyStoryPage />}/> 
+              <Route path="test-story" element={<TestPage />}/>  */}
             </Routes>
             <footer className="app-footer">
-              { <Footer /> }
+              <Footer />
             </footer>
           </main>
         </div>
